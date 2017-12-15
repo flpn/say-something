4b46224a11de75b11f5703e6a187c768959a6d28
Felipe Netto <flpncc@gmail.com>
2017-12-15 07:06:34 -0300

    Creating Record model and applying migrations

--- records/models.py
+++ records/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+
+class Record(models.Model):
+    text = models.CharField(max_length=100)
+    publication_date = models.DateTimeField()
+
+    def __str__(self):
+        return self.text
