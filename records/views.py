34a62fa2ba5a7b5c30e6e88dd9ea43721e0251f7
Felipe Netto <flpncc@gmail.com>
2017-12-15 12:45:58 -0300

    Finishing submit view

--- records/views.py
+++ records/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.views import generic 
 from django.urls import reverse
 from django.http import HttpResponseRedirect
+from django.utils import timezone
 
 from .models import Record
 
@@ -10,8 +11,27 @@ class IndexView(generic.ListView):
     template_name = 'records/index.html'
 
     def get_queryset(self):
-        return Record.objects.order_by('publication_date')[:5]
+        return Record.objects.order_by('-publication_date')[:5]
 
 
 def submit(request):
-    return HttpResponseRedirect(reverse('records:index'))
+    try:
+        record_text = request.POST['record']
+        
+        if record_text == '':
+            raise KeyError()
+
+    except KeyError:
+        record_list = Record.objects.order_by('-publication_date')[:5]
+        context = { 
+            'error_message': 'Write something before submit!',
+            'record_list': record_list
+        }
+        
+        return render(request, 'records/index.html', context)
+    
+    else:
+        record = Record(text=record_text, publication_date=timezone.now())
+        record.save()
+
+        return HttpResponseRedirect(reverse('records:index'))
